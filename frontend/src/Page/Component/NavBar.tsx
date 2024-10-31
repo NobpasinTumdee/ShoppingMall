c22d0b0151633a47ce91aff2451615474cc45d21
NobpasinTumdee <porporpor579@gmail.com>
2024-11-01 03:25:36 +0700

    add Navbar in mainWeb

---
+++ frontend/src/Page/Component/NavBar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import LOGO from "../../assets/icon/LOGO.png";
+import Clock from "../../assets/icon/ForPage/MainIcon/Clock.png";
+import Address from "../../assets/icon/ForPage/MainIcon/Address.png";
+import UserIcon from "../../assets/icon/ForPage/MainIcon/UserProfile.jpg"
+import './NavBar.css';
+
+export const NavBar: React.FC = () => {
+    return (
+        <>
+            <nav className='NavComponent'>
+                <span className='SubNab1'>
+                    <img style={{ width: '25px', height: '25px' }} src={Clock} alt="Clock" />
+                    <p>OPEN NOW <br /> 10 AM - 9 PM <br /> Now 12/05/2024 16:30</p>
+                    <div className='vertical-divider'></div>
+
+                    <img style={{ width: '25px', height: '25px' }} src={Address} alt="Address" />
+                    <p>111, University Road, Suranaree<br /> Subdistrict, Mueang Nakhon Ratchasima<br />District, Nakhon Ratchasima 30000</p>
+                    <div className='vertical-divider'></div>
+                </span>
+                <span className='SubNab2'><img style={{ width: '200px', height: '30px' }} src={LOGO} alt="LOGO" /></span>
+                <span className='SubNab3'>
+                    <span>Hello! PorGz Welcome to The Mall</span>
+                    <img style={{ width: '45px', height: '45px', borderRadius: '50px' , cursor: 'pointer'}} src={UserIcon} alt="User" />
+                </span>
+            </nav>
+            <nav className='NavComponentMenu'>
+                <div></div>
+                <div>
+                    <span className='MenuHover'>NEWS</span>
+                    <span className='MenuHover'>STORE</span>
+                    <span className='MenuHover'>BOOK A HALL</span>
+                    <span className='MenuHover'>SERVICEREQUEST</span>
+                    <span className='MenuHover'>CLEANING</span>
+                </div>
+                <div></div>
+            </nav>
+        </>
+    );
+};
--- frontend/src/Page/MainWeb/Main.tsx
+++ frontend/src/Page/MainWeb/Main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { NavBar } from '../Component/NavBar';
 const Main: React.FC = () => {
     return(
         <>
-            <h1 style={{color: "#ffffff"}}>Welcome to our MainWeb</h1>
-            <div style={{color: "#ffffff"}}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, odio totam numquam maiores quo non voluptates suscipit officiis maxime laudantium!</div>
+            <NavBar />
+            <h1 >Welcome to our MainWeb</h1>
+            <div > Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, odio totam numquam maiores quo non voluptates suscipit officiis maxime laudantium!</div>
         </>
     );
 };
