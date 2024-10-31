4cb2571da1dfbd944d35dbffe44ebd01f152d0c3
NobpasinTumdee <porporpor579@gmail.com>
2024-11-01 04:36:08 +0700

    frontend mainweb code 30%

--- frontend/src/Page/MainWeb/Main.tsx
+++ frontend/src/Page/MainWeb/Main.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
 import { NavBar } from '../Component/NavBar';
+import Product from "../../assets/icon/ForPage/MainIcon/Product.png";
+
+//Floor
+import market from "../../assets/icon/ForPage/MainIcon/Market.png"
+import Food from "../../assets/icon/ForPage/MainIcon/FoodBar.png"
+import Decorations from "../../assets/icon/ForPage/MainIcon/Home.png"
+import Computer from "../../assets/icon/ForPage/MainIcon/LaptopSettings.png"
+
+import './Main.css';
 const Main: React.FC = () => {
     return(
         <>
             <NavBar />
-            <h1 >Welcome to our MainWeb</h1>
-            <div > Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, odio totam numquam maiores quo non voluptates suscipit officiis maxime laudantium!</div>
+            <div style={{height: '110px'}}></div>
+            <div className='AdvertisingMain'>
+                <span><img src={Product} alt="Product" /></span>
+                <span className='advertisingtext'>
+                    <h1>Product advertising</h1>
+                    loremLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt  
+                    mollit anim id est laborum.
+                </span>
+            </div>
+            
+            <div className='Floor'>
+                <span>
+                    <img src={market} alt="market" />
+                    <h4>NIGHT MARKET</h4>
+                    <p>Lorem ipsum dolor sit amet, <br />consectetur adipiscing elit, sed <br />do eiusmod tempor incididunt <br />ut labore et dolore </p>
+                </span>
+                <span>
+                    <img src={Food} alt="Food" />
+                    <h4>FOOD CENTER</h4>
+                    <p>Lorem ipsum dolor sit amet, <br />consectetur adipiscing elit, sed <br />do eiusmod tempor incididunt <br />ut labore et dolore </p>
+                </span>
+                <span>
+                    <img src={Decorations} alt="Decorations" />
+                    <h4>DECORATIONS</h4>
+                    <p>Lorem ipsum dolor sit amet, <br />consectetur adipiscing elit, sed <br />do eiusmod tempor incididunt <br />ut labore et dolore </p>
+                </span>
+                <span>
+                    <img src={Computer} alt="Computer" />
+                    <h4>COMPUTER EQUIPMENT</h4>
+                    <p>Lorem ipsum dolor sit amet, <br />consectetur adipiscing elit, sed <br />do eiusmod tempor incididunt <br />ut labore et dolore </p>
+                </span>
+            </div>
+
+            <div className='NEWS'>
+                <span></span>
+                <p>NEWS</p>
+                <span></span>
+            </div>
+
+
         </>
     );
 };
