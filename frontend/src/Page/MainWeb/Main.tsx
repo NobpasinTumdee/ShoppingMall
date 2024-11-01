dd4857d07ed6313d1401a481a41699ccc6f21d62
NobpasinTumdee <porporpor579@gmail.com>
2024-11-01 15:16:34 +0700

    add New and animation in main

--- frontend/src/Page/MainWeb/Main.tsx
+++ frontend/src/Page/MainWeb/Main.tsx
@@ -8,8 +8,34 @@ import Food from "../../assets/icon/ForPage/MainIcon/FoodBar.png"
 import Decorations from "../../assets/icon/ForPage/MainIcon/Home.png"
 import Computer from "../../assets/icon/ForPage/MainIcon/LaptopSettings.png"
 
+//New
+import Newtest from "../../assets/icon/ForPage/MainIcon/TestNew.png"
+
 import './Main.css';
 const Main: React.FC = () => {
+
+    const testdata = [
+        {
+            id: 1,
+            Name: "Product advertising",
+            info: "loremLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+            id: 2,
+            Name: "Product advertising",
+            info: "loremLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+            id: 3,
+            Name: "Product advertising",
+            info: "loremLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+        {
+            id: 4,
+            Name: "Product advertising",
+            info: "loremLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+    ]
     return(
         <>
             <NavBar />
@@ -54,7 +80,28 @@ const Main: React.FC = () => {
                 <p>NEWS</p>
                 <span></span>
             </div>
+            <div className='NewInfo'>
+                {testdata.length > 0 ? (
+                    testdata.map((data) => (
+                        <div className='SubInfo' key={data.id}>
+                            <img src={Newtest} alt="Newtest" />
+                            <span>
+                                <h4>{data.Name}</h4>
+                                <p>{data.id} {data.info} </p>
+                            </span>
+                        </div>
+                    ))
+                ) : (
+                    <div>No data!</div>
+                )}
+            </div>
 
+
+            <footer>
+                <span>1</span>
+                <span>1</span>
+                <span>1</span>
+            </footer>
 
         </>
     );
