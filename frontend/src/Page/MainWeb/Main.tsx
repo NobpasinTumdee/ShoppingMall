0f39ba68a20bedbc96e230d60c37d0f2a094e95d
NobpasinTumdee <porporpor579@gmail.com>
2024-12-03 19:18:49 +0700

    testmerge

--- frontend/src/Page/MainWeb/Main.tsx
+++ frontend/src/Page/MainWeb/Main.tsx
@@ -87,6 +87,7 @@ const Main: React.FC = () => {
     const Openpopup = () => {
         setpopup(!popup)
     };
+    //main
     return(
         <>
             <div style={{height: '110px',zIndex: '0'}}></div>
