24284d52c72ffbcb0e3ca9da6e7f044ac238462c
piyathida2003 <piyathida2004@hotmail.com>
2024-12-20 02:22:53 +0700

    แก้ frontend หน้า ui แต่ยังไม่มา -1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,52 @@
-import { BrowserRouter as Router, Route, Routes ,useLocation } from 'react-router-dom';
-import Login from './Page/Login/Login';
-import Main from './Page/MainWeb/Main';
-import Admin from './Page/MainWeb/Admin/Admin';
-import AdminStore from './Page/MainWeb/Admin/AdminStore';
-import Store from './Page/MainWeb/Store/Store';
-import StorePayment from './Page/MainWeb/Store/StorePayment';
-import SubStore from './Page/MainWeb/Store/SubStore/SubStore';
-import BookStore from './Page/MainWeb/Store/SubStore/BookStore';
-import Inbox from './Page/MainWeb/Inbox/Inbox';
-import BillStore from './Page/MainWeb/Store/BillStore';
-import Recruitment from './Page/Recruitment/Recruitment';
-import AdminJob from './Page/MainWeb/Admin/AdminJob';
-import Inventory from './Page/MainWeb/Inventory/Inventory';
-import { NavBar } from './Page/Component/NavBar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Login from "./Page/Login/Login";
+import Main from "./Page/MainWeb/Main";
+import Admin from "./Page/MainWeb/Admin/Admin";
+import AdminStore from "./Page/MainWeb/Admin/AdminStore";
+import Store from "./Page/MainWeb/Store/Store";
+import StorePayment from "./Page/MainWeb/Store/StorePayment";
+import SubStore from "./Page/MainWeb/Store/SubStore/SubStore";
+import BookStore from "./Page/MainWeb/Store/SubStore/BookStore";
+import Inbox from "./Page/MainWeb/Inbox/Inbox";
+import BillStore from "./Page/MainWeb/Store/BillStore";
+import Recruitment from "./Page/Recruitment/Recruitment";
+import AdminJob from "./Page/MainWeb/Admin/AdminJob";
+import Inventory from "./Page/MainWeb/Inventory/Inventory";
+import { NavBar } from "./Page/Component/NavBar";
 
-import BookingHall from './Page/MainWeb/Hall/BookingHall/HallBookingPage';
-import Hall from './Page/MainWeb/Hall/selectHall/SelectHall';
-import CalendarPage from './Page/MainWeb/Hall/calendar/calendar';
+import BookingHall from "./Page/MainWeb/Hall/BookingHall/HallBookingPage";
+import Hall from "./Page/MainWeb/Hall/selectHall/SelectHall";
+import CalendarPage from "./Page/MainWeb/Hall/calendar/calendar";
 
-import Cleaning from './Page/MainWeb/Clearning/Cleaningpage';
+import Cleaning from "./Page/MainWeb/Clearning/Cleaningpage";
 
-import ServiceRequest from './Page/MainWeb/ServiceRequest/ServicePage';
+import ServiceRequest from "./Page/MainWeb/ServiceRequest/Request/ServicePage";
 
 const App: React.FC = () => {
   const location = useLocation();
-  const Navbar = ["/Main","/Store","/SubStore","/BookStore","/Admin","/AdminStore","/Hall","/bookinghall","/celendar","/Inbox","/StorePayment","/BillStore","/Recruitment","/AdminJob","/Inventory","/ServiceRequest"].includes(location.pathname);
+  const Navbar = [
+    "/Main",
+    "/Store",
+    "/SubStore",
+    "/BookStore",
+    "/Admin",
+    "/AdminStore",
+    "/Hall",
+    "/bookinghall",
+    "/celendar",
+    "/Inbox",
+    "/StorePayment",
+    "/BillStore",
+    "/Recruitment",
+    "/AdminJob",
+    "/Inventory",
+    "/ServiceRequest",
+  ].includes(location.pathname);
   return (
     <>
       {Navbar && <NavBar />}
@@ -36,15 +58,15 @@ const App: React.FC = () => {
         <Route path="/Store" element={<Store />} />
         <Route path="/StorePayment" element={<StorePayment />} />
         <Route path="/SubStore" element={<SubStore />} />
-        <Route path="/BookStore" element={<BookStore />} /> 
-        <Route path="/Inbox" element={<Inbox />} /> 
+        <Route path="/BookStore" element={<BookStore />} />
+        <Route path="/Inbox" element={<Inbox />} />
 
         <Route path="/BillStore" element={<BillStore />} />
         <Route path="/Hall" element={<Hall />} />
         <Route path="/bookinghall" element={<BookingHall />} />
         <Route path="/celendar" element={<CalendarPage />} />
 
-        <Route path="/BillStore" element={<BillStore />} /> 
+        <Route path="/BillStore" element={<BillStore />} />
         <Route path="/Recruitment" element={<Recruitment />} />
         <Route path="/AdminJob" element={<AdminJob />} />
         <Route path="/Inventory" element={<Inventory />} />
@@ -52,7 +74,6 @@ const App: React.FC = () => {
         <Route path="/Cleaning" element={<Cleaning />} />
 
         <Route path="/ServiceReuest" element={<ServiceRequest />} />
-
       </Routes>
     </>
   );
@@ -60,7 +81,7 @@ const App: React.FC = () => {
 
 const AppWrapper: React.FC = () => (
   <Router>
-      <App />
+    <App />
   </Router>
 );
 
---
+++ frontend/src/Page/MainWeb/ServiceRequest/Request/ServicePage.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Layout, Form, Input, Button, Select, message, DatePicker } from "antd";
+import "./ServicePage.css";
+import { ServiceInterface } from "../../../../interfaces/ServiceInterface";
+
+const { Content } = Layout;
+
+const ServicePage: React.FC = () => {
+  const [form] = Form.useForm();
+  // const [Service, setService] = useState<ServiceInterface[]>([]);
+
+  const onFinish = (values: ServiceInterface) => {
+    console.log("Success:", values);
+    message.success("Request successfully");
+    form.resetFields();
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+    message.error("Request failed!");
+  };
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Content
+        style={{
+          padding: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="form-container">
+          <h1 className="form-title">Form for Service Request</h1>
+          <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            style={{ width: "100%" }}
+          >
+            {/* Date */}
+            <Form.Item
+              label="Date"
+              name="RequestDate"
+              rules={[{ required: true, message: "Please select the date!" }]}
+            >
+              <DatePicker
+                style={{ width: "100%" }}
+                placeholder="Select the Date"
+              />
+            </Form.Item>
+
+            {/* User */}
+            <Form.Item
+              label="User"
+              name="UserID"
+              rules={[
+                { required: true, message: "Please input the user name!" },
+              ]}
+            >
+              <Input placeholder="Enter the user name" />
+            </Form.Item>
+
+            {/* Location */}
+            <Form.Item
+              label="Location"
+              name="Location"
+              rules={[
+                { required: true, message: "Please input the location!" },
+              ]}
+            >
+              <Input placeholder="Enter the location" />
+            </Form.Item>
+
+            {/* Problem */}
+            <Form.Item
+              label="Problem"
+              name="ProblemDescription"
+              rules={[
+                { required: true, message: "Please describe the problem!" },
+              ]}
+            >
+              <Input.TextArea placeholder="Describe the problem" rows={4} />
+            </Form.Item>
+
+            {/* Status */}
+            <Form.Item
+              label="Status"
+              name="StatusService"
+              rules={[
+                { required: true, message: "Please specify the status!" },
+              ]}
+            >
+              <Select placeholder="Select the status">
+                <Select.Option value="pending">Pending</Select.Option>
+                <Select.Option value="completed">Completed</Select.Option>
+              </Select>
+            </Form.Item>
+
+            {/* Submit Button */}
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="form-submit-button"
+              >
+                Send
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Content>
+    </Layout>
+  );
+};
+
+export default ServicePage;
