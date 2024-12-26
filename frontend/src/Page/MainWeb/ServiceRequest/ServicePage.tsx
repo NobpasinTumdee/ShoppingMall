ddfa6c4e8b916e100325107be8f9b997256e16c2
NobpasinTumdee <porporpor579@gmail.com>
2024-12-27 01:03:26 +0700

    service SPRINT1 เสร็จแล้ว

--- frontend/src/Page/MainWeb/ServiceRequest/ServicePage.tsx
+++ frontend/src/Page/MainWeb/ServiceRequest/ServicePage.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import {Select } from "antd";
+import {Select ,Table ,message} from "antd";
+import type { TableColumnsType } from 'antd';
 import "./ServicePage.css"
 import { StoreInterface } from '../../../interfaces/StoreInterface';
-import { CreateService, ListRepairman, ListStoreService } from '../../../services/https';
+import { CreateService, ListRepairman, ListService, ListStoreService, UpdateStoreByid } from '../../../services/https';
 import { UsersInterface } from '../../../interfaces/UsersInterface';
 import { ServiceInterface } from '../../../interfaces/ServiceInterface';
 const ServicePage: React.FC = () => {
     //=====================================API==================================
     const [Store, setStore] = useState<StoreInterface[]>([]);
     const [Repairman, setRepairman] = useState<UsersInterface[]>([]);
+    const [Service, setService] = useState<ServiceInterface[]>([]);
     useEffect(() => {
         fetchData();
+        fetchStatus('pending');
     }, []);
     const fetchData = async () => {
         try {
@@ -28,6 +31,16 @@ const ServicePage: React.FC = () => {
             }
         } catch (error) {
             setRepairman([]);
+        }
+    };
+    const fetchStatus = async (value: string) => {
+        try {
+            const res = await ListService(value);
+            if (res.status === 200 && res.data) {
+                setService(res.data);
+            }
+        } catch (error) {
+            setService([]);
         }
     };
     //===================================form===================================
@@ -91,21 +104,93 @@ const ServicePage: React.FC = () => {
             StatusService: formData.StatusService,
             RequestDate: new Date(),
         };
-        try {
-            const res = await CreateService(values);
-            if (res.status === 201) {
-                await fetchData();
+        const valuesStoreUpdate: StoreInterface = {
+            ID: formData.StoreID,
+            StatusService: 'NoRequest',
+        };
+        if (formData.StatusService === 'pending') {
+            try {
+                const res = await CreateService(values);
+                if (res.status === 201) {
+                    message.success("Send Success.");
+                    await fetchData();
+                    await fetchStatus('pending');
+                }
+            } catch (error) {
+                message.error("Send error.");
+                console.error("Error Edit:", error);
             }
-        } catch (error) {
-            console.error("Error Edit:", error);
+        }else if (formData.StatusService === 'completed') {
+            try {
+                const res = await CreateService(values);
+                if (res.status === 201) {
+                    message.success("Send completed.");
+                    await fetchData();
+                    await fetchStatus('pending');
+                }
+            } catch (error) {
+                message.error("Send error.");
+                console.error("Error Edit:", error);
+            }
+            try {
+                const res = await UpdateStoreByid(String(formData.StoreID),valuesStoreUpdate);
+                if (res.status === 200) {
+                    message.success("Update store completed.");
+                    await fetchData();
+                }
+            } catch (error) {
+                message.error("Send error.");
+            }
         }
     }
+    //======================================table=======================================
+    const columns: TableColumnsType<ServiceInterface> = [
+        {
+            title: 'StoreID',
+            dataIndex: 'StoreID',
+            width: 50,
+        },
+        {
+            title: 'UserID',
+            dataIndex: 'UserID',
+            width: 50,
+        },
+        {
+            title: 'Location',
+            dataIndex: 'Location',
+            width: 100,
+            ellipsis: true,
+        },
+        {
+            title: 'Describtion',
+            dataIndex: 'Describtion',
+            width: 150,
+            ellipsis: true,
+        },
+        {
+            title: 'RequestDate',
+            dataIndex: 'RequestDate',
+            width: 100,
+            ellipsis: true,
+        },
+        {
+            title: 'StatusService',
+            dataIndex: 'StatusService',
+            width: 70,
+            ellipsis: true,
+        },
+    ];
     return(
         <>
             <div style={{height: '110px',zIndex: '0'}}></div>
-            <div>
+            <div className='serviceanimation'>
+            <h1 className='H1Management'>Service Request</h1>
+            <div className='formService'>
                 <form onSubmit={handleSubmitEdit}>
+                    <p style={{fontWeight:'600'}}>Service Request</p>
                     <Select
+                        className="custom-ant-select"
+                        style={{width:'49%',marginRight: '1%'}}
                         showSearch
                         placeholder="Store"
                         optionFilterProp="label"
@@ -117,6 +202,8 @@ const ServicePage: React.FC = () => {
                             }))}
                     />
                     <Select
+                        className="custom-ant-select"
+                        style={{width:'50%'}}
                         showSearch
                         placeholder="Repairman"
                         optionFilterProp="label"
@@ -127,35 +214,51 @@ const ServicePage: React.FC = () => {
                             label: data.UserName || "Unknown",
                             }))}
                     />
-                    <p>Location</p>
-                    <p>{LocationStr}</p>
+                    <p style={{fontWeight:'600'}}>Location</p>
+                    <p style={{color:'#bea66a'}}>{LocationStr || "-- Please select Store --"}</p>
+                    <label style={{fontWeight:'600'}}>Describtion</label>
                     <textarea 
+                        style={{width:'97%',borderRadius: '10px',borderColor:'#CFCFCF',padding:'10px',height:'150px'}}
                         name="Describtion" 
                         id="Describtion" 
                         value={formData.Describtion}
                         onChange={handleChange}
                         required={true}
                     />  
-                    <Select
-                        showSearch
-                        placeholder="Select a Status"
-                        optionFilterProp="label"
-                        onChange={onChangeStatus}
-                        onSearch={onSearchStatus}
-                        options={[
-                        {
-                            value: 'pending',
-                            label: 'pending',
-                        },
-                        {
-                            value: 'completed',
-                            label: 'completed',
-                        },
-                        ]}
-                    />
-                    <button>Confirm</button>
+                    <p style={{fontWeight:'600'}}>Status</p>
+                    <div style={{display: 'flex',justifyContent:'space-between'}}>
+                        <Select
+                            style={{width:'50%'}}
+                            className="custom-ant-select"
+                            showSearch
+                            placeholder="Select a Status"
+                            optionFilterProp="label"
+                            onChange={onChangeStatus}
+                            onSearch={onSearchStatus}
+                            options={[
+                            {
+                                value: 'pending',
+                                label: 'pending',
+                            },
+                            {
+                                value: 'completed',
+                                label: 'completed',
+                            },
+                            ]}
+                        />
+                        <button className='buttonConfirm'>Send</button>
+                    </div>
                 </form>
                 {/* {formData.StoreID}{formData.UserID}{formData.Location}{formData.Describtion}{formData.StatusService} */}
+            </div>
+            <div style={{margin:'0 0 15%'}}>
+                <h2 style={{margin:'0 20%',fontFamily:'"Abel", sans-serif'}}>Service Request List</h2>
+                <div style={{display:'flex',justifyContent:'start',margin:'0 20%'}}>
+                    <p className='selectStatusService' onClick={() => fetchStatus('pending')}>pending</p>
+                    <p className='selectStatusService' onClick={() => fetchStatus('completed')}>completed</p>
+                </div>
+                <Table<ServiceInterface> columns={columns} dataSource={Service} size="middle" style={{width:"60%",margin:'auto'}} />
+            </div>
             </div>
         </>
     );
