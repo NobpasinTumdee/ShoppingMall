ba9f9d77e00aee1ed3274aeafa3e4efddab523b0
NobpasinTumdee <porporpor579@gmail.com>
2024-12-26 22:36:51 +0700

    ui frontend service แบบโครงใช้งานได้แต่ไม่สวยเดะไปทำต่อ

--- frontend/src/Page/MainWeb/ServiceRequest/ServicePage.tsx
+++ frontend/src/Page/MainWeb/ServiceRequest/ServicePage.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {Select } from "antd";
 import "./ServicePage.css"
 import { StoreInterface } from '../../../interfaces/StoreInterface';
-import { ListRepairman, ListStoreService } from '../../../services/https';
+import { CreateService, ListRepairman, ListStoreService } from '../../../services/https';
 import { UsersInterface } from '../../../interfaces/UsersInterface';
+import { ServiceInterface } from '../../../interfaces/ServiceInterface';
 const ServicePage: React.FC = () => {
     //=====================================API==================================
     const [Store, setStore] = useState<StoreInterface[]>([]);
@@ -29,27 +30,81 @@ const ServicePage: React.FC = () => {
             setRepairman([]);
         }
     };
+    //===================================form===================================
+    const [formData, setFormData] = useState({
+        StoreID: 0,
+        UserID: 0,
+        Location: '',
+        Describtion: '',
+        StatusService: '',
+    });
     //===================================selector===============================
-    const selectStore = (value: string) => {
+    const [Location, setLocation] = useState<StoreInterface| null>(null);
+    //store
+    const selectStore = (value: number) => {
+        const selectedStore = Store.find(store => store.ID === value);
+        setLocation(selectedStore || null);
+        formData.Location = LocationStr;
+        formData.StoreID = value;
         console.log(`selected ${value}`);
     };
-      
     const onSearchStore = (value: string) => {
         console.log('search:', value);
     };
-
-    const selectRepairman = (value: string) => {
+    //repairman
+    const selectRepairman = (value: number) => {
+        formData.UserID = value;
         console.log(`selected ${value}`);
     };
-      
     const onSearchRepairman = (value: string) => {
         console.log('search:', value);
     };
+    //Status
+    const onChangeStatus = (value: string) => {
+        formData.StatusService = value;
+        console.log(`selected ${value}`);
+    };
+    const onSearchStatus = (value: string) => {
+        console.log('search:', value);
+    };
+    // Generate LocationStr
+    const LocationStr = useMemo(() => {
+        if (!Location) return "";
+        return `Zone ${Location?.ProductType?.NameType || ""} Area ${Location?.ProductType?.NameType || ""}${Location?.ProductType?.ID || ""}${Location?.ID || ""}`;
+    }, [Location]);
+    //
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    //===================================Create=================================
+    const handleSubmitEdit = async (e: any) => {
+        e.preventDefault();
+        CreateServicefuc(formData);
+    };
+    const CreateServicefuc = async (formData: any) => {
+        const values: ServiceInterface = {
+            StoreID: formData.StoreID,
+            UserID: formData.UserID,
+            Location: LocationStr,
+            Describtion: formData.Describtion,
+            StatusService: formData.StatusService,
+            RequestDate: new Date(),
+        };
+        try {
+            const res = await CreateService(values);
+            if (res.status === 201) {
+                await fetchData();
+            }
+        } catch (error) {
+            console.error("Error Edit:", error);
+        }
+    }
     return(
         <>
             <div style={{height: '110px',zIndex: '0'}}></div>
             <div>
-                <form>
+                <form onSubmit={handleSubmitEdit}>
                     <Select
                         showSearch
                         placeholder="Store"
@@ -57,7 +112,7 @@ const ServicePage: React.FC = () => {
                         onChange={selectStore}
                         onSearch={onSearchStore}
                         options={Store.map((data) => ({
-                            value: data.ID?.toString() || "",
+                            value: data.ID || 0,
                             label: data.NameStore || "Unknown",
                             }))}
                     />
@@ -68,11 +123,39 @@ const ServicePage: React.FC = () => {
                         onChange={selectRepairman}
                         onSearch={onSearchRepairman}
                         options={Repairman.map((data) => ({
-                            value: data.ID?.toString() || "",
+                            value: data.ID || 0,
                             label: data.UserName || "Unknown",
                             }))}
                     />
+                    <p>Location</p>
+                    <p>{LocationStr}</p>
+                    <textarea 
+                        name="Describtion" 
+                        id="Describtion" 
+                        value={formData.Describtion}
+                        onChange={handleChange}
+                        required={true}
+                    />  
+                    <Select
+                        showSearch
+                        placeholder="Select a Status"
+                        optionFilterProp="label"
+                        onChange={onChangeStatus}
+                        onSearch={onSearchStatus}
+                        options={[
+                        {
+                            value: 'pending',
+                            label: 'pending',
+                        },
+                        {
+                            value: 'completed',
+                            label: 'completed',
+                        },
+                        ]}
+                    />
+                    <button>Confirm</button>
                 </form>
+                {/* {formData.StoreID}{formData.UserID}{formData.Location}{formData.Describtion}{formData.StatusService} */}
             </div>
         </>
     );
