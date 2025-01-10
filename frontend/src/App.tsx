17fdd6465e2a3b7a05821764a1da4330d7a343af
Kongsavanh12 <jame.kongsawan@gmail.com>
2025-01-11 00:19:17 +0700

    new

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,21 +16,13 @@ import { NavBar } from './Page/Component/NavBar';
 
 import BookingHall from './Page/MainWeb/Hall/BookingHall/HallBookingPage';
 import Hall from './Page/MainWeb/Hall/selectHall/SelectHall';
-<<<<<<< Updated upstream
-import Carlendar from './Page/MainWeb/Hall/carlendar/calendar';
+import Calendar from './Page/MainWeb/Hall/calendar/calendar';
 import Listbooking from './Page/MainWeb/Hall/ListBooking/ListBooking';
-
-import Cleaning from './Page/MainWeb/Clearning/Cleaningpage';
-const App: React.FC = () => {
-  const location = useLocation();
-  const Navbar = ["/Main","/Store","/SubStore","/BookStore","/Admin","/AdminStore","/Hall","/Inbox","/StorePayment","/BillStore","/Recruitment","/AdminJob","/Inventory"].includes(location.pathname);
-=======
-import CalendarPage from './Page/MainWeb/Hall/calendar/calendar';
 import Cleaning from './Page/MainWeb/Clearning/Cleaningpage';
 const App: React.FC = () => {
   const location = useLocation();
   const Navbar = ["/Main","/Store","/SubStore","/BookStore","/Admin","/AdminStore","/Hall","/Bookinghall","/celendar","/Inbox","/StorePayment","/BillStore","/Recruitment","/AdminJob","/Inventory"].includes(location.pathname);
->>>>>>> Stashed changes
+
   return (
     <>
       {Navbar && <NavBar />}
@@ -48,7 +40,7 @@ const App: React.FC = () => {
         <Route path="/BillStore" element={<BillStore />} />
         <Route path="/Hall" element={<Hall />} />
         <Route path="/booking/:id" element={<BookingHall />} />
-        <Route path="/calendar/:id" element={<Carlendar />} />
+        <Route path="/calendar/:id" element={<Calendar />} />
         <Route path="/listbooking/:id" element={<Listbooking />}/>
 
         <Route path="/BillStore" element={<BillStore />} /> 
