42beab9fb96a5af21c2eab18b121633fef59e18d
SnakeEyes <wichitchai63@gmail.com>
2025-01-19 10:21:33 +0700

    update entity 19/01/2025

--- backend/entity/InventoryRequest.go
+++ backend/entity/InventoryRequest.go
@@ -9,10 +9,25 @@ type InventoryRequest struct {
 	gorm.Model
 	NameItem string `json:"NameItem"` 
 	DateRequest time.Time    `json:"DateRequest"` 
-	QuantityRequest    int       `json:"QuantityRequest"`    
+	QuantityRequest    int       `json:"QuantityRequest"`
+
 
 	UserID       uint      `json:"UserID"`
 	User         User      `gorm:"foreignKey:UserID"`
 
-	Inventorys      []Inventory `gorm:"many2many:RequestDetails;joinForeignKey:RequestID;joinReferences:InventoryID"` 
+	// ความสัมพันธ์กับ RequestDetail
+	RequestDetails []RequestDetail `gorm:"foreignKey:RequestID"` 
+}
+
+type RequestDetail struct {
+	gorm.Model
+
+	InventoryID uint      `json:"InventoryID"`
+	Inventory   Inventory `gorm:"foreignKey:InventoryID"`
+
+	RequestID uint             `json:"RequestID"`
+	Request   InventoryRequest `gorm:"foreignKey:RequestID"`
+
+	StatusRequest bool `json:"StatusRequest"`
+	Reason string `json:"Reason"`
 }
